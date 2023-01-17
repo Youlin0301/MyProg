9d2c5a671b9b968edd1ae8d9bf07273013f9ee72
Youlin0301 <122856015+Youlin0301@users.noreply.github.com>
2023-01-17 13:51:22 +0800

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+
+class background_task
+{
+public:
+        void operator()() const
+        {
+        }
+};
+
+int main() {
+	// your code goes here
+	
+	background_task f;
+	thread my_thread(f);
+	
+	//thread my_thread(background_task());
+	
+	return 0;
+}
